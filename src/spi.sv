ab325bc4adf50ab6e48a76e05f8bb3aebca0028c
Paul Campbell <paul@taniwha.com>
2024-10-28 07:00:50 +1300

    reset spi state

--- src/spi.sv
+++ src/spi.sv
@@ -46,11 +46,18 @@ module spi(input clk, input reset,
 	
 		for (I = 0; I < 3; I = I+1) begin
 			always @(posedge clk)
+			if (reset) begin
+				r_mode[I] <= 0;
+				r_src[I] <= 0;
+			end else
 			if (reg_write && reg_sel == I && reg_addr == 4) begin
 				r_mode[I] <= reg_data_in[1:0];
 				r_src[I] <= reg_data_in[2];
 			end
 			always @(posedge clk)
+			if (reset) begin
+				r_clk_count[I] <= 0;
+			end else
 			if (reg_write && reg_sel == I && reg_addr == 5) begin
 				r_clk_count[I] <= reg_data_in;
 			end
